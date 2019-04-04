ccbe893918f35790d781dd046a6e06fb8579d93a
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2019-04-04 11:53:49 +0530

    Move files

---
+++ vscode-plugin/src/artifacts/artifactResolver.ts
@@ -0,0 +1,120 @@
+'use strict';
+
+import { window } from "vscode";
+import { ArtifactModule } from "./ArtifactModule";
+import {APIArtifactInfo, ProxyArtifactInfo} from "./artifactUtils";
+
+export async function createArtifact(artifactType: string) {
+    let option: number | undefined;
+    switch(artifactType) {
+        case "apiOld": {
+            option = await showQuickPickForArtifactCreationOptions(APIArtifactInfo.CREATE_API, APIArtifactInfo.IMPORT_API, APIArtifactInfo.PLACEHOLDER_API);
+            if(option && option === 1) {
+                const destinationFileName = await window.showInputBox({ value: "", prompt: "Enter Synapse API Artifact Name", placeHolder: "Enter"}).then(text => text);
+                
+                if(destinationFileName) {
+                    ArtifactModule.createTemplate(APIArtifactInfo.API_DESTINATION_FOLDER, "api", destinationFileName);
+                }
+            }else if(option && option === 2) {
+                //resolce import case
+            }
+            break;
+        }
+        case "proxyOld": {
+            option = await showQuickPickForArtifactCreationOptions(ProxyArtifactInfo.CREATE_PROXY, ProxyArtifactInfo.IMPORT_PROXY, ProxyArtifactInfo.PLACEHOLDER_PROXY);
+            if(option && option === 1) {
+                const proxyTypes = createProxyServiceArray();
+                const selectedArifactType = await showQuickPick(proxyTypes);
+                
+                if(selectedArifactType) {
+                    const artifactName = await window.showInputBox({ value: "", prompt: "Enter Synapse Proxy Artifact Name", placeHolder: "Enter"}).then(text => text);
+                    if(artifactName) {
+                        ArtifactModule.createTemplate(ProxyArtifactInfo.PROXY_DESTINATION_FOLDER, selectedArifactType, artifactName);
+                    }
+                }
+            }else if(option && option === 2) {
+                //resolce import case
+            }
+            break;
+        }  
+        case "api": {
+            const destinationFileName = await window.showInputBox({ value: "", prompt: "Enter Synapse API Artifact Name", placeHolder: "Enter"}).then(text => text);
+            if(destinationFileName) {
+                ArtifactModule.createTemplate(APIArtifactInfo.API_DESTINATION_FOLDER, "api", destinationFileName);
+            }
+            break;
+        } 
+        case "proxy": {
+            const proxyTypes = createProxyServiceArray();
+            const selectedArifactType = await showQuickPick(proxyTypes);
+            
+            if(selectedArifactType) {
+                const artifactName = await window.showInputBox({ value: "", prompt: "Enter Synapse Proxy Artifact Name", placeHolder: "Enter"}).then(text => text);
+                if(artifactName) {
+                    ArtifactModule.createTemplate(ProxyArtifactInfo.PROXY_DESTINATION_FOLDER, selectedArifactType, artifactName);
+                }
+            }
+            break;
+        }
+    }
+}
+
+async function showQuickPick(promise: Promise<{"value": string; "label": string;}[]>): Promise<string | undefined> {
+    return await window.showQuickPick(
+        promise.then(items => items.map(item => ({
+            value: item.value,
+            label: item.label,
+            description: "",
+            detail: ""
+        }))),
+        { matchOnDescription: true, placeHolder: "Select type..." }
+    ).then(selected => (
+        selected && selected.value
+    ));
+}
+
+async function showQuickPickForArtifactCreationOptions(create: string, imports: string, placeHolder: string): Promise<number | undefined> {
+    return await window.showQuickPick(
+        createQuickPickArray(create, imports).then(items => items.map(item => ({
+            value: item.id,
+            label: item.name,
+            description: "",
+            detail: ""
+        }))),
+        { matchOnDescription: true, placeHolder: placeHolder }
+    ).then(selected => selected && selected.value);
+}
+
+async function showQuickPickForArtifactCreationOptionsNew(create: string, imports: string, placeHolder: string): Promise<number | undefined> {
+    return await window.showQuickPick(
+        createQuickPickArray(create, imports).then(items => items.map(item => ({
+            value: item.id,
+            label: item.name,
+            description: "",
+            detail: ""
+        }))),
+        { matchOnDescription: true, placeHolder: placeHolder }
+    ).then(selected => selected && selected.value);
+}
+
+function createQuickPickArray(create: string, imports: string): Promise<{"id": number, "name":string}[]>{
+    return new Promise((resolve, reject)=>{
+        const options = [{"id": 1, "name":create}, {"id": 1, "name":imports}];
+        resolve(options);
+    });
+}
+
+function createProxyServiceArray(): Promise<{"value": string; "label": string;}[]> {
+    return new Promise((resolve, reject) => {
+        const serviceArray = [
+            {"value": ProxyArtifactInfo.PASS_THROUGH_PROXY, "label": ProxyArtifactInfo.PASS_THROUGH_PROXY_LABEL},
+            {"value": ProxyArtifactInfo.CUSTOM_PROXY, "label": ProxyArtifactInfo.CUSTOM_PROXY_LABEL},
+            {"value": ProxyArtifactInfo.TRANSFOREMER_PROXY, "label": ProxyArtifactInfo.TRANSFOREMER_PROXY_LABEL},
+            {"value": ProxyArtifactInfo.LOGGING_PROXY, "label": ProxyArtifactInfo.LOGGING_PROXY_LABEL},
+            {"value": ProxyArtifactInfo.WSDL_BASED_PROXY, "label": ProxyArtifactInfo.WSDL_BASED_PROXY_LABEL},
+            {"value": ProxyArtifactInfo.SECURE_PROXY, "label": ProxyArtifactInfo.SECURE_PROXY_LABEL}
+        ];
+        resolve(serviceArray);
+    });
+}
+
---
+++ vscode-plugin/src/artifacts/artifactUtils.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+export class APIArtifactInfo {
+    static readonly CREATE_API: string = "Create a New API Artifact";
+    static readonly IMPORT_API: string = "Import API Artifact";
+    static readonly PLACEHOLDER_API: string = "Select an API Artifact Creation Option...";
+    static readonly API_DESTINATION_FOLDER: string = "api";
+
+}
+
+export class ProxyArtifactInfo {
+    static readonly CREATE_PROXY: string = "Create a New Proxy Artifact";
+    static readonly IMPORT_PROXY: string = "Import Proxy Artifact";
+    static readonly PLACEHOLDER_PROXY: string = "Select an Proxy Artifact Creation Option...";
+
+    static readonly PROXY_DESTINATION_FOLDER: string = "proxy-services";
+    static readonly PASS_THROUGH_PROXY: string = "PassThroughProxy";
+    static readonly CUSTOM_PROXY: string = "CustomProxy";
+    static readonly TRANSFOREMER_PROXY: string = "TransformerProxy";
+    static readonly LOGGING_PROXY: string = "LoggingProxy";
+    static readonly WSDL_BASED_PROXY: string = "WSDLBasedProxy";
+    static readonly SECURE_PROXY: string = "SecureProxy";
+    
+    static readonly PASS_THROUGH_PROXY_LABEL: string = "Pass Through Proxy";
+    static readonly CUSTOM_PROXY_LABEL: string = "Custom Proxy";
+    static readonly TRANSFOREMER_PROXY_LABEL: string = "Transformer Proxy";
+    static readonly LOGGING_PROXY_LABEL: string = "Logging Proxy";
+    static readonly WSDL_BASED_PROXY_LABEL: string = "WSDL Based roxy";
+    static readonly SECURE_PROXY_LABEL: string = "Secure Proxy";
+
+}
+  
