0ff48192e545ca61da0aa8db14cb8efd7711ebc5
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2019-04-29 11:53:13 +0530

    Resolve changes

--- language-server-extensions/src/main/java/org/eclipse/lsp4xml/extensions/synapse/contentmodel/participants/SynapseContentModelCompletionParticipant.java
+++ language-server-extensions/src/main/java/org/eclipse/lsp4xml/extensions/synapse/contentmodel/participants/SynapseContentModelCompletionParticipant.java
@@ -98,7 +98,7 @@ public class SynapseContentModelCompletionParticipant extends CompletionParticip
             String documentation = child.getDocumentation();
             String name = child.getName();
 
-            if (name != null && (name.equals("sequence") || name.equals("endpoint")) && rootElement) {
+            if (name != null && "sequence".equals(name) || "endpoint".equals(name) && rootElement) {
                 name += ".parent";
             }
 
