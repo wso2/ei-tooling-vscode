963851987c10fab564eb59ebb5cb4d7b98db8856
RukshanFdo <rukshanfernando234@gmail.com>
2023-03-15 11:22:02 +0530

    Adding frontend diagrams to the project

--- diagrams/src/App.tsx
+++ diagrams/src/App.tsx
@@ -1,24 +1,31 @@
-import { vscode } from "./utilities/vscode";
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
-import "./App.css";
+import React, { useCallback, useState } from 'react';
+import './App.css';
 
-function App() {
-  function resourcesPanel() {
-    vscode.postMessage(
-      {
-        command: "resources",
-        text: "This is resources panel",
-      }
-    );
-  }
+import { FileNode, root } from './data';
 
-
+const File: React.FC<FileNode> = ({ id, children }: FileNode) => {
+  const [showChildren, setShowChildren] = useState<boolean>(false);
+  const handleClick = useCallback(() => {
+    setShowChildren(!showChildren);
+  }, [showChildren, setShowChildren])
 
   return (
-    <main>
-      <h1>Hello World!</h1>
-      <VSCodeButton onClick={resourcesPanel}>Resources</VSCodeButton>
-    </main>
+    <div>
+      <span onClick={handleClick}>
+        <h4 style={{ fontWeight: showChildren ? 'bold' : 'normal' }}>{id}</h4>
+      </span>
+      <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', left: 25, borderLeft: '1px solid', paddingLeft: 15 }}>
+        {showChildren && (children ?? []).map((node: FileNode) => <File {...node}/>)}
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div style={{ marginLeft: 15 }}>
+      <File {...root}/>
+    </div>
   );
 }
 
