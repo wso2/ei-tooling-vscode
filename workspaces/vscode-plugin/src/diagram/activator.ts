a50b3f4173946958fb8a6dc534ec2fd80f370656
chevonfdz <86297885+chevonfdz@users.noreply.github.com>
2023-06-16 14:19:35 +0530

    Update activator.ts

--- workspaces/vscode-plugin/src/diagram/activator.ts
+++ workspaces/vscode-plugin/src/diagram/activator.ts
@@ -185,7 +185,7 @@ class DiagramPanel {
                 methodName: "getFileContent",
                 handler: async (args: any[]): Promise<string | undefined> => {
                     // Get the active text editor
-                    const filePath = 'C:/Users/Chevon/Documents/vscodeAPI/extension.xml';
+                    const filePath = args[0];
                     const doc = workspace.textDocuments.find((doc) => doc.fileName === filePath);
                     if (doc) {
                         return doc.getText();
